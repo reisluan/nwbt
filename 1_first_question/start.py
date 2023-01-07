521248a3ae087906b1de50d4278a9d44890b57e9
Luan Reis <luan.reis@live.com>
2023-01-07 02:13:59 -0300

    Primeira questao resolvida

--- 1_first_question/start.py
+++ 1_first_question/start.py
@@ -1,8 +1,24 @@
+# Import libraries into the project
 import requests
+import json
 
-def consumirApi():
-    result = requests.get('https://jsonplaceholder.typicode.com/comments')
-    print(result.content)
 
+# Function to consume API
+def consume_api():
+    request = requests.get('https://jsonplaceholder.typicode.com/comments')
+    return json.loads(request.content)
+
+
+# Function to display results
+def print_mail(result_json):
+    for item in result_json:
+        tld = item['email'][-4:]
+        if tld == '.org':
+            print(item['email'])
+
+
+# Run only if file is called directly
 if __name__ == '__main__':
-    consumirApi()
+    result = consume_api()
+    print_mail(result)
+
